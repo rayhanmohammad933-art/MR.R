1c77bd915dc5a75dd31fb5bda2708d565c4b3c14
rayhanmohammad933-art <rayhanmohammad933@gmail.com>
2025-10-09 12:11:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolsapa = document.getElementById('sapaButton');
 tombolsapa.addEventListener('click', function() {
-  alert('hey broo! makasih loo sudah berkunjung!');
+  alert('hey bro! makasih lo sudah berkunjung!');
 });
